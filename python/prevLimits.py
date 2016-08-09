68e6d1bccfe4f4d6a5b866beefed25b088458010
Devin Taylor <dntaylor@wisc.edu>
2016-08-09 13:38:23 -0500

    add expected

--- python/prevLimits.py
+++ python/prevLimits.py
@@ -12,3 +12,17 @@ prevLimits = {
     'BP3'   : {'Hpp4lPP' : 454, 'Hpp3lAP' : 492, 'Hpp3lPP' : 458, 'HppAP': 492, 'HppPP': 512, 'HppComb' : 560,},
     'BP4'   : {'Hpp4lPP' : 407, 'Hpp3lAP' : 466, 'Hpp3lPP' : 463, 'HppAP': 466, 'HppPP': 500, 'HppComb' : 537,},
 }
+
+# cms 8 TeV expected limites
+prevExpLimits = {
+    'ee100' : {'Hpp4lPP' : 507, 'Hpp3lAP' : 517, 'Hpp3lPP' : 480, 'HppAP': 517, 'HppPP': 550, 'HppComb' : 608,},
+    'em100' : {'Hpp4lPP' : 514, 'Hpp3lAP' : 521, 'Hpp3lPP' : 494, 'HppAP': 521, 'HppPP': 568, 'HppComb' : 616,},
+    'mm100' : {'Hpp4lPP' : 530, 'Hpp3lAP' : 526, 'Hpp3lPP' : 496, 'HppAP': 526, 'HppPP': 575, 'HppComb' : 621,},
+    'et100' : {'Hpp4lPP' : 251, 'Hpp3lAP' : 336, 'Hpp3lPP' : 380, 'HppAP': 336, 'HppPP': 395, 'HppComb' : 434,},
+    'mt100' : {'Hpp4lPP' : 264, 'Hpp3lAP' : 352, 'Hpp3lPP' : 406, 'HppAP': 352, 'HppPP': 418, 'HppComb' : 454,},
+    'tt100' : {'Hpp4lPP' : 155, 'Hpp3lAP' : 120, 'Hpp3lPP' :   1, 'HppAP': 120, 'HppPP': 155, 'HppComb' : 175,}, # 7 TeV!
+    'BP1'   : {'Hpp4lPP' : 351, 'Hpp3lAP' : 428, 'Hpp3lPP' : 427, 'HppAP': 428, 'HppPP': 454, 'HppComb' : 504,},
+    'BP2'   : {'Hpp4lPP' : 433, 'Hpp3lAP' : 480, 'Hpp3lPP' : 473, 'HppAP': 480, 'HppPP': 512, 'HppComb' : 557,},
+    'BP3'   : {'Hpp4lPP' : 454, 'Hpp3lAP' : 491, 'Hpp3lPP' : 457, 'HppAP': 491, 'HppPP': 512, 'HppComb' : 560,},
+    'BP4'   : {'Hpp4lPP' : 407, 'Hpp3lAP' : 464, 'Hpp3lPP' : 461, 'HppAP': 464, 'HppPP': 498, 'HppComb' : 536,},
+}
